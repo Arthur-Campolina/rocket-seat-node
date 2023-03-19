7a412a41beeed62a8ea81e9cd14a898a4ba692af
Arthur Campolina <arthurcampolina@hotmail.com>
2023-03-18 21:08:10 -0300

    feat: cria projeto, rooutes e server

---
+++ Desafio/src/routes.js
@@ -0,0 +1,44 @@
+export const routes = [
+    {
+        method: 'GET',
+        path: '/tasks',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'GET',
+        path: '/tasks/:id',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'POST',
+        path: '/tasks',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/tasks/:id',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'PATCH',
+        path: '/tasks/:id/complete',
+        handler: (req, res) => {
+
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/tasks/:id',
+        handler: (req, res) => {
+
+        }
+    },
+]
---
+++ Desafio/src/server.js
@@ -0,0 +1,8 @@
+import http from 'node:http'
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+})
+
+server.listen(3333)
