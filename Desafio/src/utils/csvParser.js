3cfee00b9e7c5cf4fff40fb04f670108da3f50ad
Arthur Campolina <arthurcampolina@hotmail.com>
2023-03-23 13:01:17 -0300

    feat: arquivo CSV parser criado

--- Desafio/src/utils/csvParser.js
+++ Desafio/src/utils/csvParser.js
@@ -1,3 +1,28 @@
-export function csvParser() {
+import fs from 'node:fs'
+import csv from 'csv-parser'
+import { Transform, Writable } from 'node:stream'
+
+export async function csvParser(data) {
+    if (!data) return console.log('No data found!')
+    const readableStream = fs.createReadStream(data)
+    const transformStreamToObject = csv({ separator: ':' })
+    const transformStreamToString = new Transform({
+        objectMode: true,
+        transform(chunk, encoding, callback) {
+            callback(null, JSON.stringfy(chunk))
+        },
+    })
+    const writabelStream = new Writable({
+        write(chunk, encoding, callback) {
+            const string = chunk.toString()
+            const data = JSON.parse(string)
+        }
+    })
 
 }
+console.log('CSV parsing start!', Date())
+readableStream
+    .pipe(transformStreamToObject)
+    .pipe(transformStreamToString)
+    .pipe(writabelStream)
+    .on('close', () => console.log('CSV parsing end!', Date()))
