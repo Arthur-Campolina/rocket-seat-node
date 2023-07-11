e1a00254f2df8148b40fa24104e01025c57d55ac
Arthur Campolina <arthurcampolina@hotmail.com>
2023-07-10 22:19:37 -0300

    feat: cria e2etest environment + organiza NPM test scripts

--- Aulas/03-api-solid/src/services/test/getNumberCheckInsService.spec.ts
+++ Aulas/03-api-solid/src/services/test/getNumberCheckInsService.spec.ts
@@ -1,14 +1,14 @@
 import { InMemoryCheckInRepository } from "@/repositories/in-memory/inMemoryCheckInRepository";
 import { describe, beforeEach, it, expect } from "vitest";
-import { GetNumberCheckInsByUser } from "../getNumberCheckInsService";
+import { GetNumberCheckInsService } from "../getNumberCheckInsService";
 
 let checkInRepository: InMemoryCheckInRepository;
-let sut: GetNumberCheckInsByUser;
+let sut: GetNumberCheckInsService;
 
 describe("Get Number of Checkins by User Service", () => {
   beforeEach(async () => {
     checkInRepository = new InMemoryCheckInRepository();
-    sut = new GetNumberCheckInsByUser(checkInRepository);
+    sut = new GetNumberCheckInsService(checkInRepository);
   });
 
   it("Should Get Number of CheckIns by User", async () => {
